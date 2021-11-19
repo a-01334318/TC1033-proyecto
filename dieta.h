247a62c7d77cfc4c488d240244627d15fc0b0fca
a-01334318 <94150935+a-01334318@users.noreply.github.com>
2021-11-18 21:36:38 -0600

    Update and rename dietas.h to dieta.h

--- dieta.h
+++ dieta.h
@@ -17,20 +17,23 @@ private:
   float calorias;
   float proteinas;
   float lipidos;
+  float carbs;
 
 public:
   Dieta();
-  Dieta(std::string tip, float cal, float pro, float lip): tipo(tip), calorias(cal), proteinas(pro), lipidos(lip){};
+  Dieta(std::string tip, float cal, float pro, float lip, float car): tipo(tip), calorias(cal), proteinas(pro), lipidos(lip), carbs(car){};
 
   std::string get_tipo();
   float get_calorias();
   float get_proteinas();
   float get_lipidos();
+  float get_carbs();
 
   void set_tipo(std::string );
   void set_calorias(float );
   void set_proteinas(float );
   void set_lipidos(float );
+  void set_carbs(float );
 
   float calxcomida(int );
 
@@ -67,6 +70,14 @@ float Dieta::get_lipidos(){
 
 void Dieta::set_lipidos(float lip){
   lipidos = lip;
+}
+
+float Dieta::get_carbs(){
+  return carbs;
+}
+
+void Dieta::set_carbs(float car){
+  carbs = car;
 }
 
 float Dieta::calxcomida(int veces){

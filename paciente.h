0ea77dde56a6ec8bbbbd2054121708439b9fd498
a-01334318 <94150935+a-01334318@users.noreply.github.com>
2021-11-25 22:39:55 -0600

    Update paciente.h

--- paciente.h
+++ paciente.h
@@ -4,32 +4,39 @@
 * Proyecto Nutricion
 * Programar la clase Paciente que:
 * Atributos: nombre, peso, estatura, edad y sexo
-* Métodos: getters(), setters(), imc(tiempo)
-* Constructor
+* Métodos: constructor, getters(), setters(), imc(tiempo)
+* Herencia: Hipertension y Colitis
 */
+#ifndef PACIENTE_H
+#define PACIENTE_H
 
-#include<string>
+#include <string>
+#include <iostream>
+#include "objetivo.h"
+#include "dieta.h"
+
+using namespace std;
 
 class Paciente{
-private:
-  std::string nombre;
-  std::string sexo;
+protected:
+  string nombre;
+  string sexo;
   float peso;
   float estatura;
   int edad;
 
 public:
   Paciente();
-  Paciente(std::string nom, std::string se, float pe, float es, int ed): nombre(nom), sexo(se), peso(pe), estatura(es), edad(ed){};
+  Paciente(string nom, string se, float pe, float es, int ed): nombre(nom), sexo(se), peso(pe), estatura(es), edad(ed){};
 
-  std::string get_nombre();
-  std::string get_sexo();
+  string get_nombre();
+  string get_sexo();
   float get_peso();
   float get_estatura();
   int get_edad();
 
-  void set_nombre(std::string );
-  void set_sexo(std::string );
+  void set_nombre(string );
+  void set_sexo(string );
   void set_peso(float );
   void set_estatura(float );
   void set_edad(int );
@@ -39,19 +46,19 @@ public:
 };
 
 
-std::string Paciente::get_nombre(){
+string Paciente::get_nombre(){
   return nombre;
 }
 
-void Paciente::set_nombre(std::string nom){
+void Paciente::set_nombre(string nom){
   nombre = nom;
 }
 
-std::string Paciente::get_sexo(){
+string Paciente::get_sexo(){
   return sexo;
 }
 
-void Paciente::set_sexo(std::string se){
+void Paciente::set_sexo(string se){
   sexo = se;
 }
 
@@ -83,3 +90,91 @@ float Paciente::imc(){
   float imc = peso / (estatura * estatura);
   return imc;
 }
+
+// Herencia Hipertension
+class Hipertension: public Paciente{
+protected:
+  bool tratamiento;
+  float colesterol;
+
+public:
+  Hipertension();
+  Hipertension(string nom, string se, float pe, float es, int ed, bool tra, float col):Paciente(nom, se, pe, es, ed){
+    tratamiento = tra;
+    colesterol = col;
+  }
+
+  bool get_tratamiento();
+  float get_colesterol();
+
+  void set_tratamiento(bool );
+  void set_colesterol(float );
+
+};
+
+bool Hipertension::get_tratamiento(){
+  return tratamiento;
+}
+
+void Hipertension::set_tratamiento(bool tra){
+  tratamiento = tra;
+}
+
+float Hipertension::get_colesterol(){
+  return colesterol;
+}
+
+void Hipertension::set_colesterol(float col){
+  colesterol = col;
+}
+
+// Herencia Colitis
+class Colitis: public Paciente{
+protected:
+  bool tratamiento;
+  string subtipo;
+  float hierro;
+
+public:
+  Colitis();
+  Colitis(string nom, string se, float pe, float es, int ed, bool tra, string sub, float fe):Paciente(nom, se, pe, es, ed){
+    tratamiento = tra;
+    subtipo = sub;
+    hierro = fe;
+  }
+
+  bool get_tratamiento();
+  string get_subtipo();
+  float get_hierro();
+
+  void set_tratamiento(bool );
+  void set_subtipo(string );
+  void set_hierro(float );
+
+};
+
+bool Colitis::get_tratamiento(){
+  return tratamiento;
+}
+
+void Colitis::set_tratamiento(bool tra){
+  tratamiento = tra;
+}
+
+string Colitis::get_subtipo(){
+  return subtipo;
+}
+
+void Colitis::set_subtipo(string sub){
+  subtipo = sub;
+}
+
+float Colitis::get_hierro(){
+  return hierro;
+}
+
+void Colitis::set_hierro(float fe){
+  hierro = fe;
+}
+
+#endif

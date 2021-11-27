626c5aecd695ce7b5ad12638441f1cda95ca6169
a-01334318 <94150935+a-01334318@users.noreply.github.com>
2021-11-26 19:15:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,54 +18,70 @@ using namespace std;
 
 int main(){
 
+/* A implementar en próxima entrega
+*  cout << "Menu:\n";
+*  cout << "1. Crea paciente. \n";
+*  cout << "2. Mostrar pacientes. \n";
+*  cout << "3. Mostrar pacientes con hipertension. \n";
+*  cout << "4. Mostrar pacientes con colitis. \n";
+*  cout << "5. Calcular total de pacientes activos. \n";
+*  cout << "6. Mostrar dietas[n] de paciente. \n";
+*  cout << "7. Mostrar objetivos[n] de paciente. \n";
+*  cout << "8. Agregar dietas[n] a paciente. \n";
+*  cout << "9. Agregar objetivo[n] a paciente. \n";
+*  cout << "10. Salir \n";
+*/
+
   Paciente paciente_1("Raquel", "f", 55.0, 1.6, 58);
-  cout<<paciente_1.get_nombre()<< " "
-      <<paciente_1.get_sexo()<< " "
-      <<paciente_1.get_peso()<< " "
-      <<paciente_1.get_estatura()<< " "
-      <<paciente_1.get_edad()<<endl;
+  cout << paciente_1.get_nombre() << " "
+       << paciente_1.get_sexo() << " "
+       << paciente_1.get_peso() << " "
+       << paciente_1.get_estatura() << " "
+       << paciente_1.get_edad()<< endl;
       paciente_1.set_nombre("Monica");
       paciente_1.set_peso(57.0);
-      cout<<paciente_1.imc()<<endl;
+      cout << paciente_1.imc() << endl;
 
   Dieta dieta_1("Keto1", 1600, 30, 70, 20);
-  cout<<dieta_1.get_tipo()<< " "
-      <<dieta_1.get_calorias()<< " "
-      <<dieta_1.get_proteinas()<< " "
-      <<dieta_1.get_lipidos()<< " "
-      <<dieta_1.get_carbs()<<endl;
+  cout << dieta_1.get_tipo() << " "
+       << dieta_1.get_calorias() << " "
+       << dieta_1.get_proteinas() << " "
+       << dieta_1.get_lipidos() << " "
+       << dieta_1.get_carbs() <<endl;
 
       dieta_1.set_tipo("Keto2");
       dieta_1.set_calorias(1700);
-      cout<< dieta_1.calxcomida(3)<<endl;
+      cout << dieta_1.calxcomida(3) << endl;
 
   Objetivo objetivo_1("Subir", 2.0, 1);
-  cout<<objetivo_1.get_tipo()<< " "
-      <<objetivo_1.get_cantidad()<< " "
-      <<objetivo_1.get_tiempo()<<endl;
+  cout << objetivo_1.get_tipo() << " "
+       << objetivo_1.get_cantidad() << " "
+       << objetivo_1.get_tiempo() << endl;
 
        objetivo_1.set_tiempo(3);
-       cout<<objetivo_1.kilosalmes()<<endl;
+       cout << objetivo_1.kilosalmes() << endl;
 
+// Ejemplo 1 Herencia
   Hipertension paciente_2("Rafael", "m", 70.0, 1.64, 58, true, 200.9);
-  cout<<paciente_2.get_nombre()<< " "
-      <<paciente_2.get_peso()<< " "
-      <<paciente_2.get_tratamiento()<< " "
-      <<paciente_2.get_colesterol()<<endl;
+  cout << paciente_2.get_nombre() << " "
+       << paciente_2.get_peso() << " "
+       << paciente_2.get_tratamiento() << " "
+       << paciente_2.get_colesterol() << endl;
 
        paciente_2.set_colesterol(187.5);
-       cout<<paciente_2.get_colesterol()<<endl;
-
+       cout << paciente_2.get_colesterol() << endl;
+       
+// Ejemplo 2 Herencia
   Colitis paciente_3("Erik", "m", 71.0, 1.80, 24, false, "SII", 100.0);
-  cout<<paciente_3.get_nombre()<< " "
-      <<paciente_3.get_sexo()<< " "
-      <<paciente_3.get_peso()<< " "
-      <<paciente_3.get_estatura()<< " "
-      <<paciente_3.get_edad()<< " "
-      <<paciente_3.get_tratamiento()<< " "
-      <<paciente_3.get_subtipo()<< " "
-      <<paciente_3.get_hierro()<<endl;
+  cout << paciente_3.get_nombre() << " "
+       << paciente_3.get_sexo() << " "
+       << paciente_3.get_peso() << " "
+       << paciente_3.get_estatura() << " "
+       << paciente_3.get_edad() << " "
+       << paciente_3.get_tratamiento() << " "
+       << paciente_3.get_subtipo() << " "
+       << paciente_3.get_hierro() <<endl;
 
   return 0;
-  
+
 }

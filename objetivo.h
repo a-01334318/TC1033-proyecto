6179943ed07fa5976251d5211cf0a7ce313ddff6
a-01334318 <94150935+a-01334318@users.noreply.github.com>
2021-12-02 21:59:34 -0600

    Update objetivo.h

--- objetivo.h
+++ objetivo.h
@@ -4,8 +4,9 @@
 * Proyecto Nutrición
 * Programar la clase Objetivo que:
 * Atributos: tipo, cantidad, tiempo
-* Métodos: constructor, getters(), setters(), kilosalmes()
+* Métodos: constructor, getters(), setters(), kilosalmes(), toString()
 */
+
 #ifndef OBJETIVO_H
 #define OBJETIVO_H
 
@@ -14,57 +15,121 @@
 
 using namespace std;
 
+//Declaración de clase Objetivo
 class Objetivo{
-private:
-  string tipo;
-  float cantidad;
-  int tiempo;
 
-public:
-  Objetivo(): tipo(""), cantidad(0), tiempo(0){};
-  Objetivo(string tip, float can, int tie): tipo(tip), cantidad(can), tiempo(tie){};
+  //Declaro atributos protegidos
+  protected:
+    string tipo;
+    float cantidad;
+    int tiempo;
 
-  string get_tipo();
-  float get_cantidad();
-  int get_tiempo();
+  //Declaro métodos públicos
+  public:
+    //Constructor por default
+    Objetivo(): tipo(""), cantidad(0), tiempo(0){};
+    //Constructor con parámetros
+    Objetivo(string tip, float can, int tie): tipo(tip), cantidad(can), tiempo(tie){};
 
+    //getters
+    string get_tipo();
+    float get_cantidad();
+    int get_tiempo();
 
-  void set_tipo(string );
-  void set_cantidad(float );
-  void set_tiempo(int );
+    //setters
+    void set_tipo(string );
+    void set_cantidad(float );
+    void set_tiempo(int );
 
-  float kilosalmes();
-
+    //métodos extra
+    float kilosalmes();
+    string toString()const;
 };
 
+/**
+ * getter tipo
+ *
+ * @param
+ * @return string: tipo de objetivo
+*/
 string Objetivo::get_tipo(){
   return tipo;
 }
 
+/**
+ * setter tipo
+ *
+ * @param string tip: tipo de objetivo
+ * @return
+*/
 void Objetivo::set_tipo(string tip){
   tipo = tip;
 }
 
+/**
+ * getter cantidad
+ *
+ * @param
+ * @return float: cantidad de kilos
+*/
 float Objetivo::get_cantidad(){
   return cantidad;
 }
 
+/**
+ * setter cantidad
+ *
+ * @param float can: cantidad de kilos
+ * @return
+*/
 void Objetivo::set_cantidad(float can){
   cantidad = can;
 }
 
+/**
+ * getter tiempo
+ *
+ * @param
+ * @return int tiempo: tiempo para alcanzar objetivo
+*/
 int Objetivo::get_tiempo(){
   return tiempo;
 }
 
+/**
+ * setter tiempo
+ *
+ * @param int tie: tiempo para alcanzar objetivo
+ * @return
+*/
 void Objetivo::set_tiempo(int tie){
   tiempo = tie;
 }
 
+/**
+ * Metodo que calcula los kilos al mes, divide la cantidad de kilos
+ * entre el tiempo para alcanzar objetivo (meses).
+ *
+ * @param
+ * @return float de meta de kilos al mes
+*/
 float Objetivo::kilosalmes(){
   float meta;
   meta = cantidad / tiempo;
   return meta;
+}
+
+/**
+ * Almacenda los valores de las variables de instancia en una cadena de
+ * texto.
+ *
+ * @param
+ * @return string con los datos del Objetivo
+*/
+string Objetivo::toString()const{
+  stringstream aux;
+  aux << "Tipo:" << tipo << " Kilos a subir/bajar:" << cantidad << " Tiempo:" << tiempo << "\n";
+  return aux.str();
 }
 
 #endif
